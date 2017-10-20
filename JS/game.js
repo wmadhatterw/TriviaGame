9238405fcbe6e9c814b9739f83d5b06d1a565516
Seth Walton <sdwalton83@gmail.com>
2017-10-20 00:15:12 -0500

    update

--- JS/game.js
+++ JS/game.js
@@ -8,11 +8,15 @@ window.onload = function() {
 		 window.location.reload(true);
 	});
 	// $("#start").click($("#wrapper").show());
-	$("#submit").click(function(){
-		
+	$(".submit").click(function(){
+		var empty = 0;
 		var one = $("input[type='radio'][name='1']:checked");
 		var oneVal =  one.val();
-		if (oneVal !== "Shane") {
+
+		if (oneVal == undefined) {
+			empty++;
+		}
+		else if (oneVal !== "Shane") {
 			losses++;
 		} else {
 			wins++
@@ -20,86 +24,114 @@ window.onload = function() {
 
 		var two = $("input[type='radio'][name='2']:checked");
 		var twoVal = two.val();
-		if (twoVal !== "Shane") {
+		if (twoVal == undefined) {
+			empty++;
+		}
+		else if (twoVal !== "Shane") {
 			losses++;
 		} else{
 			wins++;
 		}
 		var three = $("input[type='radio'][name='3']:checked");
 		var threeVal = three.val();
-		if (threeVal !== "Her boyfriend and her friend") {
+		if (threeVal == undefined) {
+			empty++;
+		}
+		else if (threeVal !== "Her boyfriend and her friend") {
 			losses++;
 		} else {
 			wins++
 		}
 		var four = $("input[type='radio'][name='4']:checked");
 		var fourVal = four.val();
-		if (fourVal !== "A deer") {
+		if (fourVal == undefined) {
+			empty++;
+		}
+		else if (fourVal !== "A deer") {
 			losses++;
 		} else {
 			wins++
 		}
 		var five = $("input[type='radio'][name='5']:checked");
 		var fiveVal =five.val();
-		if (fiveVal !== "Clear") {
+		if (fiveVal == undefined) {
+			empty++;
+		}
+		else if (fiveVal !== "Clear") {
 			losses++;
 		} else {
 			wins++
 		}
 		var six = $("input[type='radio'][name='6']:checked");
 		var sixVal =six.val();
-		if (sixVal !== "Penny") {
+		if (sixVal == undefined) {
+			empty++;
+		}
+		else if (sixVal !== "Penny") {
 			losses++;
 		} else {
 			wins++
 		}
 		var seven = $("input[type='radio'][name='7']:checked");
 		var sevenVal = seven.val();
-		if (sevenVal !== "Eugene") {
+		if (sevenVal == undefined) {
+			empty++;
+		}
+		else if (sevenVal !== "Eugene") {
 			losses++;
 		} else {
 			wins++
 		}
 		var eight = $("input[type='radio'][name='8']:checked");
 		var eightVal =eight.val();
-		if (eightVal !== "Pregnancy Test") {
+		if (eightVal == undefined) {
+			empty++;
+		}
+		else if (eightVal !== "Pregnancy Test") {
 			losses++;
 		} else {
 			wins++
 		}
 		var nine = $("input[type='radio'][name='9']:checked");
 		var nineVal = nine.val();
-		if (nineVal !== "Maggie") {
+		if (nineVal == undefined) {
+			empty++;
+		}
+		else if (nineVal !== "Maggie") {
 			losses++;
 		} else {
 			wins++
 		}
 		var ten = $("input[type='radio'][name='10']:checked");
 		var tenVal = ten.val()
-		if (tenVal !== "Baseball Bat") {
+		if (tenVal == undefined) {
+			empty++;
+		}
+		else if (tenVal !== "Baseball Bat") {
 			losses++;
 		} else {
 			wins++
 		}
-		console.log(losses);
 		console.log(wins);
+		console.log(losses);
+		console.log(empty);
 		$("#begin").css("display", "none");
 		$("#submit").css("display", "none");
+		$("html,body").scrollTop(0);
 		$("#quiz").css("display", "none");
-		$("#end").html("Wins: " + wins + "<br>Losses: " + losses);
+		$("#end").html("Wins: " + wins + "<br>Losses: " + losses + "<br>UnAnswered: " + empty);
 		$("#box").css("background", "url(../TriviaGame/images/glenn.jpeg)");
 		$("#box").css("background-size", "100%");
 
 
-// "<div id ='Buttons'><button id='submit'>Submit</button><button id='restart'>Restart</button></div>")
-// 		$("#end").text("Wins: " + wins);
-// 		$("#end").text("Losses: " + losses);
+
 	});
-}
+}//end of window load
+
+
 document.getElementById("begin").addEventListener("click", myFunction);
 var wins = 0;
 var losses = 0;
-var empty = 0;
 function myFunction() {
     $("#quiz").css("display", "inline-block");
     load();
